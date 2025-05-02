9ce30c0520faf42d9ecbfb7c6a3f20387a16fc1e
John Liman <john.liman@monash.edu>
2025-05-02 12:28:14 +1000

    Fixed the bug when it is a checkbox it will not check property

--- AutoCompleteStatusRequiredFieldsModule.php
+++ AutoCompleteStatusRequiredFieldsModule.php
@@ -5,7 +5,6 @@ namespace Monash\Helix\AutoCompleteStatusRequiredFieldsModule;
 use ExternalModules\AbstractExternalModule;
 use ExternalModules\ExternalModules;
 use REDCap;
-
 class AutoCompleteStatusRequiredFieldsModule extends AbstractExternalModule
 {
     function redcap_save_record($project_id, $record, $instrument, $event_id, $group_id, $survey_hash, $response_id, $repeat_instance)
@@ -16,65 +15,77 @@ class AutoCompleteStatusRequiredFieldsModule extends AbstractExternalModule
         }
 
         $settings = ExternalModules::getProjectSettingsAsArray($this->PREFIX, $project_id);
-        $instrumentsToBeChecked = $settings['instruments_to_be_checked']['value'] ?? [];
-        $currentCompleteStatus = $_POST[$instrument . '_complete'] ?? null;
+        $instrumentsToBeChecked = $settings['instruments_to_be_checked']['value'];
+        // get the current value of 'complete'
 
-        // Skip if it's a delete or the instrument isn't in the list to check
-        if (!isset($currentCompleteStatus) || (!is_null($instrumentsToBeChecked[0]) && !in_array($instrument, $instrumentsToBeChecked))) {
-            return;
-        }
+        $currentCompleteStatusValue = $_POST[$instrument . '_complete'];
 
-        $newStatus = $this->checkRequiredFields($instrument);
-        if ($newStatus === $currentCompleteStatus) {
-            return; // No update needed
-        }
+        // do not do the below if it is a delete action, ie $currentCompleteStatusValue is not set
+        if (isset($currentCompleteStatusValue) && (is_null($instrumentsToBeChecked[0]) || in_array($instrument, $instrumentsToBeChecked))) {
+            $newStatus = $this->checkRequiredFields($instrument);
 
-        global $Proj;
-        $recordIdField = REDCap::getRecordIdField();
-        $eventName = REDCap::getEventNames(true, true, $event_id);
+            // if it is the same, do not update it
+            if ($newStatus == $currentCompleteStatusValue) {
+                return;
+            }
 
-        $data = [
-            $recordIdField => $record,
-            'redcap_event_name' => $eventName,
-            $instrument . '_complete' => $newStatus
-        ];
+            // if it is different name
+            $redcapRecordIdFieldName = REDCap::getRecordIdField();
+            $redcapEventName = REDCap::getEventNames(true, true, $event_id);
 
-        // Add repeating form or event info if applicable
-        if ($Proj->isRepeatingForm($event_id, $instrument)) {
-            $data['redcap_repeat_instrument'] = $instrument;
-            $data['redcap_repeat_instance'] = $repeat_instance;
-        } elseif ($Proj->isRepeatingEvent($event_id)) {
-            $data['redcap_repeat_instance'] = $repeat_instance;
-        }
+            global $Proj;
 
-        $jsonData = json_encode([$data]);
-        $saveResponse = REDCap::saveData($project_id, 'json', $jsonData, 'overwrite');
-
-        $errorsExist = (
-            (is_array($saveResponse['errors']) && count($saveResponse['errors']) > 0) ||
-            (!is_array($saveResponse['errors']) && !empty($saveResponse['errors']))
-        );
-
-        if ($errorsExist) {
-            $errors = print_r($saveResponse['errors'], true);
-            $message = sprintf(
-                "The %s could not save %s form status because of the following error(s):\n\n%s",
-                $this->getModuleName(),
-                $instrument,
-                $errors
+            $arrVarNames = array_merge(
+                array($redcapRecordIdFieldName => $record,
+                    'redcap_event_name' => $redcapEventName,
+                    $instrument . '_complete' => $newStatus
+                )
             );
 
-            error_log($message);
-            REDCap::logEvent($this->getModuleName(), $errors, '', $record, $event_id);
-        } else {
-            // Save to REDCap log
-            $detail = ($newStatus === 2)
-                ? "All required fields in '$instrument' are entered"
-                : "Not all required fields in '$instrument' are entered";
+            if ($Proj->isRepeatingForm($event_id, $instrument)) {
+                $arrVarNames = array_merge($arrVarNames,
+                    array(
+                        'redcap_repeat_instrument' => $instrument,
+                        'redcap_repeat_instance' => $repeat_instance
+                    )
+                );
+            }
+            else if ($Proj->isRepeatingEvent($event_id)) {
+                $arrVarNames = array_merge($arrVarNames,
+                    array(
+                        'redcap_repeat_instance' => $repeat_instance
+                    )
+                );
+            }
 
-            REDCap::logEvent($this->getModuleName(), $detail, '', $record, $event_id);
+            $jsonData = json_encode(array($arrVarNames));
+            $saveResponse = REDCap::saveData($project_id, 'json', $jsonData, 'overwrite');
+
+            $errorsExist = (
+                (is_array($saveResponse['errors']) && count($saveResponse['errors']) > 0) ||
+                (!is_array($saveResponse['errors']) && !empty($saveResponse['errors']))
+            );
+
+            if ($errorsExist) {
+                $errors = print_r($saveResponse['errors'], true);
+                $message = sprintf(
+                    "The %s could not save %s form status because of the following error(s):\n\n%s",
+                    $this->getModuleName(),
+                    $instrument,
+                    $errors
+                );
+
+                error_log($message);
+                REDCap::logEvent($this->getModuleName(), $errors, '', $record, $event_id);
+            } else {
+                // Save to REDCap log
+                $detail = ($newStatus === 2)
+                    ? "All required fields in '$instrument' are entered"
+                    : "Not all required fields in '$instrument' are entered";
+
+                REDCap::logEvent($this->getModuleName(), $detail, '', $record, $event_id);
+            }
         }
-
     }
 
     // The below code is an adaptation of DataEntry::checkReqFields
@@ -83,95 +94,80 @@ class AutoCompleteStatusRequiredFieldsModule extends AbstractExternalModule
     private function checkRequiredFields($instrument)
     {
         global $Proj;
-        $emptyReqFields = $_POST['empty-required-field'] ?? [];
 
-        foreach ($Proj->forms[$instrument]['fields'] as $field => $label) {
-            if (!$this->isRequiredField($field, $emptyReqFields, $Proj)) {
-                continue;
+        // Loop through each to check if required
+        foreach ($Proj->forms[$instrument]['fields'] as $this_field=>$this_label)
+        {
+            // this is for PHP 8 fixes
+            $emptyReqFields = array();
+            if (isset($_POST['empty-required-field'])) {
+                $emptyReqFields = $_POST['empty-required-field'];
             }
-            if ($this->isFieldMissing($field, $Proj)) {
-                return 0; // Incomplete
-            }
-        }
-        return 2; // Complete
-    }
+            // Only check field's value if the field is required and not part of hidden required fields
+            if ($Proj->metadata[$this_field]['field_req'] && !in_array($this_field, $emptyReqFields))
+            {
+                // skip the @HIDDEN tags except the @HIDDEN-SURVEY tag
+                $actionTags = str_replace(array("\r", "\n", "\t"), array(" ", " ", " "), $Proj->metadata[$this_field]['misc']);
+                $actionTagsArr = explode(" ", $actionTags);
+                if (array_intersect($actionTagsArr, ['@HIDDEN', '@HIDDEN-FORM'])) {
+                    continue;
+                }
 
-    private function isRequiredField($field, $emptyReqFields, $Proj)
-    {
-        $isRequired = $Proj->metadata[$field]['field_req'] ?? false;
-        $isNotEmptyRequired = !in_array($field, $emptyReqFields);
+                // Init the missing flag
+                $isMissing = false;
 
-        if (!$isRequired || !$isNotEmptyRequired) {
-            return false;
-        }
+                // Do check for non-checkbox fields
+                if (isset($_POST[$this_field]) && !$Proj->isCheckbox($this_field) && $_POST[$this_field] == '')
+                {
+                    $isMissing = true;
+                }
+                // Do check for checkboxes, making sure at least one checkbox is checked
+                elseif ($Proj->isCheckbox($this_field) && !isset($_POST["__chkn__".$this_field]))
+                {
+                    $checkboxEnum = parseEnum($Proj->metadata[$this_field]['element_enum']);
+                    foreach (array_keys($checkboxEnum) as $key) {
+                        $checkboxFieldName = "__chk__" . $this_field . "_RC_" . str_replace("|", ".", $key);
+                        if (isset($_POST[$checkboxFieldName]) && $_POST[$checkboxFieldName] == '') {
+                            // one of the checkbox is empty
+                            // we only say is missing if all are empty
+                            $isMissing = true;
+                        } else {
+                            // if any one of them has value, we set them not missing and exit the loop
+                            $isMissing = false;
+                            break;
+                        }
+                    }
+                }
 
-        // Check for HIDDEN and HIDDEN-FORM tags (but not HIDDEN-SURVEY)
-        $actionTags = str_replace(["\r", "\n", "\t"], " ", $Proj->metadata[$field]['misc'] ?? '');
-        $actionTagsArr = explode(" ", $actionTags);
-
-        return !array_intersect($actionTagsArr, ['@HIDDEN', '@HIDDEN-FORM']);
-    }
-
-    private function isFieldMissing($field, $Proj)
-    {
-        // Non-checkbox field
-        if (!$Proj->isCheckbox($field)) {
-            return $_POST[$field] === '';
-            // the below statement will not work when there is a randomised field in the form because the randomised
-            // field value will not be passed back when we save the form
-            //return !isset($_POST[$field]) || $_POST[$field] === '';
-        }
-
-        // Checkbox field
-        if (!isset($_POST["__chkn__{$field}"])) {
-            $enum = parseEnum($Proj->metadata[$field]['element_enum']);
-            foreach (array_keys($enum) as $key) {
-                $checkboxField = "__chk__{$field}_RC_" . str_replace("|", ".", $key);
-                if (!empty($_POST[$checkboxField])) {
-                    return false; // At least one checkbox is selected
+                if ($isMissing)
+                {
+                    return 0; // form status field = 'Incomplete'
                 }
             }
-            return true; // All checkboxes are empty
         }
-        return false;
+        return 2; // form status field = 'Complete'
     }
 
     function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id, $group_id, $repeat_instance)
     {
         $settings = ExternalModules::getProjectSettingsAsArray($this->PREFIX, $project_id);
+        $statusFieldAction = $settings['status_field_action']['value'];
 
-        $statusFieldAction = $settings['status_field_action']['value'] ?? '';
-        $instrumentsToBeChecked = $settings['instruments_to_be_checked']['value'] ?? [];
+        $instrumentsToBeChecked = $settings['instruments_to_be_checked']['value'];
 
-        // Exit early if instrument is not in the list to be checked
-        if (!is_null($instrumentsToBeChecked[0]) && !in_array($instrument, $instrumentsToBeChecked)) {
-            return;
-        }
-
-        $jsInstrument = json_encode($instrument); // safely encode for JS
-
-        switch ($statusFieldAction) {
-            case 'disable':
-                echo "<script>
-                $(document).ready(function() {
-                    $('[name=' + $jsInstrument + '_complete]').prop('disabled', true);
-                });
-            </script>";
-                break;
-
-            case 'hidden':
-            default:
-                echo "<script>
-                $(document).ready(function() {
-                    $('#' + $jsInstrument + '_complete-sh-tr').hide();
-                    $('#' + $jsInstrument + '_complete-tr').hide();
-                });
-            </script>";
-                break;
-
-            case 'as':
-                // Do nothing
-                break;
+        if (is_null($instrumentsToBeChecked[0]) || in_array($instrument, $instrumentsToBeChecked)) {
+            if ($statusFieldAction == "as")
+            {
+                // do nothing
+            }
+            else if ($statusFieldAction == "disable")
+            {
+                echo '<script>$(document).ready(function() { $("[name=\'' . $instrument . '_complete\'").prop("disabled", "disabled"); });</script>';
+            }
+            else { // hidden - making it as the default for backward compatibility
+                echo '<script>$(document).ready(function() { $("#' . $instrument . '_complete-sh-tr").hide(); $("#' . $instrument . '_complete-tr").hide(); });</script>';
+            }
         }
     }
+
 }
